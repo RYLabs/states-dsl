75b445f64f165e905cc8ba667db170791b2249e3
Calvin Yu <me@sourcebender.com>
2018-03-18 17:09:56 -0400

    Allow Comment on state machine

--- lib/states/dsl/state_machine.rb
+++ lib/states/dsl/state_machine.rb
@@ -13,6 +13,16 @@ module States
         @context.register_trait(name, trait)
       end
 
+      def comment(comment)
+        @comment = comment
+      end
+
+      def serializable_hash
+        h = super
+        h["Comment"] = @comment if @comment
+        h
+      end
+
       def to_json
         JSON.pretty_generate(serializable_hash)
       end
